0f969edac859a19e81bd030108ec8e009df506ea
Alejandro Gonzalez <alexgonzanu@gmail.com>
2021-02-20 12:46:27 +0100

    incorporado funciones en el codigo para dividir mejor

--- js/analiza-palabras.js
+++ js/analiza-palabras.js
@@ -1,5 +1,36 @@
 /* Crea tu función aquí */
-const array = ["hola", "alejandro", "fusible", "amable"];
+const deletrearPalabra = (arrayPalabras, contador) => {
+  let deletreo = "";
+  for (let i = 0; i < arrayPalabras[contador].length; i++) {
+    if (i == 0) {
+      deletreo = arrayPalabras[contador].charAt(i);
+    } else {
+      deletreo += "-" + arrayPalabras[contador].charAt(i);
+    }
+  }
+  return deletreo;
+}
+
+const mayusculaOrNotAndParOrNot = (arrayPalabras, contador) => {
+  let mayusculaOrNot = arrayPalabras[contador].charAt(0);
+  if (arrayPalabras[contador].length % 2 === 0 && mayusculaOrNot !== mayusculaOrNot.toUpperCase()) {
+    console.log("La palabra es par y no empieza por mayúscula");
+  } else if (arrayPalabras[contador].length % 2 === 0 && mayusculaOrNot === mayusculaOrNot.toUpperCase()) {
+    console.log("La palabra es par y empieza por mayúscula");
+  } else if (arrayPalabras[contador].length % 2 !== 0 && mayusculaOrNot === mayusculaOrNot.toUpperCase()) {
+    console.log("La palabra es impar y empieza por mayúscula");
+  } else {
+    console.log("La palabra es impar y no empieza por mayúscula");
+  }
+}
+
+const containsPalabra = (arrayPalabras, contador) => {
+  if (arrayPalabras[contador].substr(-3) == "ble") {
+    console.log("La palabra contiene 'ble'");
+  } else {
+    console.log("La palabra no contiene 'ble'");
+  }
+}
 
 const analizaPalabras = (arrayPalabras) => {
   for (let contador in arrayPalabras) {
@@ -8,31 +39,10 @@ const analizaPalabras = (arrayPalabras) => {
     /* Punto 2 */
     console.log(`Nº de caracteres: ${arrayPalabras[contador].length}`);
     /* Punto 3 */
-    let deletreo = "";
-    for (let i = 0; i < arrayPalabras[contador].length; i++) {
-      if (i == 0) {
-        deletreo = arrayPalabras[contador].charAt(i);
-      } else {
-        deletreo += "-" + arrayPalabras[contador].charAt(i);
-      }
-    }
-    console.log(`Deletreo: ${deletreo}`);
+    console.log(`Deletreo: ${deletrearPalabra(arrayPalabras, contador)}`);
     /* Punto 4 */
-    let mayusculaOrNot = arrayPalabras[contador].charAt(0);
-    if (array[contador].length % 2 === 0 && mayusculaOrNot !== mayusculaOrNot.toUpperCase()) {
-      console.log("La palabra es par y no empieza por mayúscula");
-    } else if (array[contador].length % 2 === 0 && mayusculaOrNot === mayusculaOrNot.toUpperCase()) {
-      console.log("La palabra es par y empieza por mayúscula");
-    } else if (array[contador].length % 2 !== 0 && mayusculaOrNot === mayusculaOrNot.toUpperCase()) {
-      console.log("La palabra es impar y empieza por mayúscula");
-    } else {
-      console.log("La palabra es impar y no empieza por mayúscula");
-    }
+    mayusculaOrNotAndParOrNot(arrayPalabras, contador);
     /* Punto 5 */
-    if (array[contador].substr(-3) == "ble") {
-      console.log("La palabra contiene 'ble'");
-    } else {
-      console.log("La palabra no contiene 'ble'");
-    }
+    containsPalabra(arrayPalabras, contador);
   }
 }
